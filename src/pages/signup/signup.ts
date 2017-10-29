47eeee3d4be2459e032e3a1f7eb5e97ddd415750
Jose Tapia <jose.rikleo.tapia@gmail.com>
2017-10-29 18:09:49 +0100

    Initial commit

---
+++ src/pages/signup/signup.ts
@@ -0,0 +1,31 @@
+import { HomePage } from './../home/home';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the SignupPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-signup',
+  templateUrl: 'signup.html',
+})
+export class SignupPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad SignupPage');
+  }
+
+  signup() {
+    // Api connection
+    this.navCtrl.push(HomePage);
+  }
+
+}
